ce46abec696096262eb87b63bfc82792c25a8f3d
Akintola <akinlekan28@yahoo.com>
2018-11-21 18:13:38 +0100

    wrapped up graphql root query

--- schema.js
+++ schema.js
@@ -32,7 +32,34 @@ const RootQuery = new GraphQLObjectType({
             type: new GraphQLList(LunchType),
             resolve(parent, args){
                 return axios.get('https://api.spacexdata.com/v3/launches/')
-                .then(res => res.data)
+                .then(res => res.data);
+            }
+        },
+        lunch: {
+            type: LunchType,
+            args: {
+                flight_number: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                .then(res => res.data);
+            }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args){
+                return axios.get('https://api.spacexdata.com/v3/rockets/')
+                .then(res => res.data);
+            }
+        },
+        rocket: {
+            type: RocketType,
+            args: {
+                id: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                return axios.get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
+                .then(res => res.data);
             }
         }
     }
