13d6b54331a79399580f7d07885376d939caa87b
Akintola <akinlekan28@yahoo.com>
2018-12-11 11:18:10 +0100

    Added client template

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import logo from "./logo.jpg";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <img
+            src={logo}
+            alt="spaceX"
+            style={{
+              width: 300,
+              display: "block",
+              height: "80px",
+              margin: "auto"
+            }}
+          />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
